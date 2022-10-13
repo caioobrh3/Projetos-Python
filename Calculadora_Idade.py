a0130c2f399ba71aceb3100d8aaf995c2f62db3c
Caio Ricardo <contatocaioobr@hotmail.com>
2022-10-13 10:43:00 -0300

    correção

--- Calculadora_Idade.py
+++ Calculadora_Idade.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
-from tkcalendar import Calendar,DateEntry
-from dateutil import relativedelta
+from tkcalendar import Calendar, DateEntry
+from dateutil.relativedelta import relativedelta
 from datetime import date
 
 janela = Tk()
