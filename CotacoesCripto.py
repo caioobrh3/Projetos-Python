f30673030a02d7709d706d27a5fb90a1a2ed2efe
Caio Ricardo <contatocaioobr@hotmail.com>
2022-10-16 19:23:15 -0300

    Create CotacoesCripto.py

---
+++ CotacoesCripto.py
@@ -0,0 +1,32 @@
+import requests
+from tkinter import *
+
+def pegar_cotacoes():
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+    requisicao_dic = requisicao.json()
+
+    cotacao_dolar = requisicao_dic['USDBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
+    cotacao_btc = requisicao_dic['BTCBRL']['bid']
+
+    texto = f'''
+    Dólar: {cotacao_dolar}
+    Euro: {cotacao_euro}
+    BTC: {cotacao_btc}'''
+
+    texto_cot["text"] = texto
+
+janela = Tk()
+janela.title("Cotação atual moedas")
+
+texto_orientcao = Label(janela, text="Clique para ver as cotações")
+texto_orientcao.grid(column=0,row=0)
+
+botao = Button(janela,text="Buscar cotações",command=pegar_cotacoes)
+botao.grid(column=0,row=1)
+
+texto_cot = Label(janela,text="")
+texto_cot.grid(column=0,row=2)
+
+janela.mainloop()
