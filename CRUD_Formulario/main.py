b4cc7edc1dc1b1146a3c36e2edb4a2ba10c38ac5
Caio Ricardo <contatocaioobr@hotmail.com>
2022-10-18 17:26:27 -0300

    CRUD

---
+++ CRUD_Formulario/main.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+# Cores
+co0 = "#f0f3f5"  # Preta
+co1 = "#feffff"  # branca
+co2 = "#4fa882"  # verde
+co3 = "#38576b"  # valor
+co4 = "#403d3d"   # letra
+co5 = "#e06636"   # - profit
+co6 = "#038cfc"   # azul
+co7 = "#ef5350"   # vermelha
+co8 = "#263238"   # + verde
+co9 = "#e9edf5"   # sky blue
+
+janela = Tk()
+janela.title()
+janela.geometry("1043x453")
+janela.configure(bg=co9)
+janela.resizable(width=FALSE,height=FALSE)
+
+# Frames
+
+
+janela.mainloop()
