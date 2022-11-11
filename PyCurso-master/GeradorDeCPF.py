03483e4580a4537674659ade337ccac2c1879849
Caio Ricardo <contatocaioobr@hotmail.com>
2022-11-11 15:01:38 -0300

    Update GeradorDeCPF.py

--- PyCurso-master/GeradorDeCPF.py
+++ PyCurso-master/GeradorDeCPF.py
@@ -1,24 +1,24 @@
 from random import randint
-numero = str(randint(100000000,999999999))
 
-novo_cpf = numero
-reverso = 10
-total = 0
+def gerar():
+    numero = str(randint(100000000,999999999))
 
-for index in range(19):
-    if index > 8:
-        index -= 9
+    novo_cpf = numero
+    reverso = 10
+    total = 0
 
-    total += int(novo_cpf[index]) * reverso
+    for index in range(19):
+        if index > 8:
+            index -= 9
 
-    reverso -= 1
-    if reverso < 2:
-        reverso = 11
-        d = 11 - (total % 11)
+        total += int(novo_cpf[index]) * reverso
 
-        if d > 9:
-            d = 0
-        total = 0
-        novo_cpf += str(d)
+        reverso -= 1
+        if reverso < 2:
+            reverso = 11
+            d = 11 - (total % 11)
 
-print(novo_cpf)
+            if d > 9:
+                d = 0
+            total = 0
+            novo_cpf += str(d)
